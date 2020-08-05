88a02d9995fca47d9c08a24d6fa05343e5ec3c5a
guilhermemzlima <guilherme.munhozlima@gmail.com>
2020-08-05 20:28:27 -0300

    base project

---
+++ src/main/java/com/beerhouse/beer/BeerController.java
@@ -0,0 +1,10 @@
+package com.beerhouse.beer;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("beer")
+public class BeerController {
+
+}
